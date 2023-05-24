7f0135285adf48b489281f5ad07401fb925849c4
spencerckhuang <shuan126@jh.edu>
2023-05-24 11:02:51 -0400

    layout for workoutTracker screen plus addition of an isVisible state

--- src/screens/WorkoutTracker.js
+++ src/screens/WorkoutTracker.js
@@ -6,17 +6,76 @@ import Footer from '../components/Footer';
 
 const WorkoutTracker = ({ navigation, props }) => {
 
+    const [isVisible, setIsVisible] = useState(false);
+
+    const [exerciseName, setExerciseName] = useState('');
+    const [weight, setWeight] = useState('');
+    const [sets, setSets] = useState('');
+    const [reps, setReps] = useState('');
+
+    const handlePress = () => {  // handle press of add new exercise button
+        setIsVisible(true);
+    };
+
+    const handleSubmit = () => { // handle submit after user enters data
+        // TODO
+    };
+
+
     return (
         <View style={styles.screen}>
             <Header screenName="Tracker"/>
             
             <View style={styles.body}>
                 <Text style={ {fontSize: 30, fontWeight: '600'}}>Tracker</Text>
-            </View>
+                
+                <TouchableOpacity
+                    onPress={handlePress}
+                >
+                    <Text>Add new exercise</Text>
+                </TouchableOpacity>
 
-            {/* <View>
-                <Text>Day of workout: {props.date}</Text>
-            </View> */}
+                {isVisible && (
+                    <View styles={styles.userInputSection}>
+                        {/*  Some of these are def not right, maybe make new hooks in here? */}
+                        <TextInput
+                            style={styles.userInput}
+                            placeholder="Enter exercise name"
+                            onChangeText={text => setExerciseName(text)}
+                            value={exerciseName}
+                        />
+
+                        <TextInput
+                            style={styles.userInput}
+                            placeholder="Enter weight"
+                            onChangeText={text => setWeight(text)}
+                            value={weight}
+                        />
+
+                        <TextInput
+                            style={styles.userInput}
+                            placeholder="Enter sets"
+                            onChangeText={text => setSets(text)}
+                            value={sets}
+                        />
+
+                        <TextInput
+                            style={styles.userInput}
+                            placeholder="Enter reps"
+                            onChangeText={text => setReps(text)}
+                            value={reps}
+                        />
+
+                        <TouchableOpacity
+                            onPress={handleSubmit}
+                        >
+                            <Text>Submit</Text>
+                        </TouchableOpacity>
+                    
+                    </View>
+                )}
+
+            </View>
 
             <Footer navigation={navigation}/>
         </View>
@@ -33,6 +92,9 @@ const styles = StyleSheet.create({
         flex: 13,       // in order for header/footer to have constant height, body must be flex 13
         justifyContent: 'center',
         alignItems: 'center',
+
+        borderWidth: 3,
+        borderColor: 'red',
     },
     footer: {
         flex: 1
