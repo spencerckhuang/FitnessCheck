ce78d6d8139e280620f2d160ac6b83a8b465289e
spencerckhuang <shuan126@jh.edu>
2023-07-22 14:49:13 -0400

    laying out foundations in backend

--- fitness_tracker_backend/workout_tracker/views.py
+++ fitness_tracker_backend/workout_tracker/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+from django.http.request import HttpRequest
 
-# Create your views here.
+class WorkoutView(APIView):
+    def get():
+        return
+
+    def post():
+        return
+    
+    def put():
+        return
+    
+    def delete():
+        return
+
